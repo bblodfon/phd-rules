924f576ae9577fbfaebadf252f840db48d14297a
john <bblodfon@gmail.com>
2021-07-15 23:52:42 +0200

    build pdf and epub

--- build_book.sh
+++ build_book.sh
@@ -1,3 +1,5 @@
 Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::epub_book')"
 xdg-open docs/index.html
 
